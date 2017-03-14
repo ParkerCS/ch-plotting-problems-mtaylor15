ba1e500fc6ff26744ec353663f15da2a216cf42b
marctaylor <mtaylor@fwparker.org>
2017-03-14 11:10:42 -0500

    done

--- chi_pub_lib_problem.py
+++ chi_pub_lib_problem.py
@@ -7,3 +7,32 @@
 # plot the total visitors on the y and month on the x.  You will have 4 separate lines (total and 3 libraries)
 # add a legend
 # label axes, title the graph
+
+import re
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import csv
+
+
+def split_line(line):
+    return re.findall('[A-Za-z0-9]+(?:\'[A-Za-a0-9]+)?', line)
+
+
+library_list = []
+file = open("chilib_vistors_2016")
+reader = csv.reader(file, delimiter='\t')
+for line in reader:
+    library_list.append(line)
+
+headers = library_list[0]
+library_list = library_list[1:]
+
+print(file)
+
+for line in file:
+    words = split_line(line)
+    library_list.append(words)
+print(library_list)
+
+
+
